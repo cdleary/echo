966f36284b37abca7949ba331f8c7223600a3e73
Christopher D. Leary <cdleary@gmail.com>
2019-11-23 13:12:08 -0800

    Build out AST formatter.

--- src/echo/fuzz/genseq.py
+++ src/echo/fuzz/genseq.py
@@ -1,57 +1,108 @@
 import enum
+import textwrap
 from typing import Tuple, Text, Any, Sequence
 
 
-class StatementKind(enum.Enum):
+INDENT_PER_LEVEL = 4
+
+
+class StmtKind(enum.Enum):
     ASSIGN = 'assign'
     EXPR = 'expr'
+    CLASS_DEF = 'class-def'
+    FN_DEF = 'fn-def'
+    RETURN = 'return'
+    PASS = 'pass'
 
 
 class NameDef:
     def __init__(self, name: Text):
         self.name = name
 
+    def format(self) -> Text:
+        return self.name
+
     def __str__(self) -> Text:
         return self.name
 
 
-class Statement:
+class Stmt:
     @classmethod
-    def make_assign(cls, ident: NameDef, rhs: 'Expr') -> 'Statement':
-        return cls(StatementKind.ASSIGN, (ident, rhs))
+    def make_return(cls, operand: 'Expr') -> 'Stmt':
+        return cls(StmtKind.RETURN, (operand,))
 
     @classmethod
-    def make_expr(cls, arg: 'Expr') -> 'Statement':
-        return cls(StatementKind.EXPR, (arg,))
+    def make_pass(cls) -> 'Stmt':
+        return cls(StmtKind.PASS, ())
 
-    def __init__(self, kind: StatementKind, operands: Tuple[Any, ...]):
+    @classmethod
+    def make_class_def(cls, name: NameDef, body: Tuple['Stmt', ...]) -> 'Stmt':
+        assert isinstance(body, tuple), body
+        return cls(StmtKind.CLASS_DEF, (name, body))
+
+    @classmethod
+    def make_fn_def(cls, name: NameDef, args: Tuple[NameDef, ...],
+                    body: Tuple['Stmt', ...]) -> 'Stmt':
+        return cls(StmtKind.FN_DEF, (name, args, body))
+
+    @classmethod
+    def make_assign(cls, ident: NameDef, rhs: 'Expr') -> 'Stmt':
+        return cls(StmtKind.ASSIGN, (ident, rhs))
+
+    @classmethod
+    def make_expr(cls, arg: 'Expr') -> 'Stmt':
+        return cls(StmtKind.EXPR, (arg,))
+
+    def __init__(self, kind: StmtKind, operands: Tuple[Any, ...]):
         assert isinstance(operands, tuple), operands
         self.kind = kind
         self.operands = operands
 
-    def format(self) -> Text:
-        if self.kind == StatementKind.ASSIGN:
+    def __repr__(self) -> Text:
+        return f'{self.__class__.__name__}({self.kind}, {self.operands})'
+
+    def _format(self) -> Text:
+        if self.kind == StmtKind.ASSIGN:
             assert len(self.operands) == 2, self.operands
             return '{} = {}'.format(self.operands[0],
                                     self.operands[1].format())
-        if self.kind == StatementKind.EXPR:
+        if self.kind == StmtKind.EXPR:
             assert len(self.operands) == 1, self.operands
             return '{}'.format(self.operands[0].format())
+        if self.kind == StmtKind.CLASS_DEF:
+            name, body = self.operands
+            return ('class {}:\n'.format(name) +
+                    '\n'.join(s.format(indent=INDENT_PER_LEVEL) for s in body))
+        if self.kind == StmtKind.RETURN:
+            return 'return {}'.format(self.operands[0].format())
+        if self.kind == StmtKind.PASS:
+            return 'pass'
+        if self.kind == StmtKind.FN_DEF:
+            name, args, body = self.operands
+            indent_str = ' ' * INDENT_PER_LEVEL
+            body = ('\n' + indent_str).join(s.format() for s in body)
+            args_str = ', '.join(a.format() for a in args)
+            return f'def {name}({args_str}):\n{indent_str}{body}\n'
         raise NotImplementedError(self)
+
+    def format(self, indent: int = 0) -> Text:
+        return textwrap.indent(self._format(), ' ' * indent)
 
 
 class Block:
-    def __init__(self, stmts: Sequence[Statement]):
+    def __init__(self, stmts: Sequence[Stmt]):
         self.stmts = tuple(stmts)
 
-    def format(self):
-        return '\n'.join(s.format() for s in self.stmts)
+    def format(self, indent: int = 0):
+        return '\n'.join(s.format() for s in self.stmts) + '\n'
 
 
 class ExprKind(enum.Enum):
     INVOKE = 'invoke'
     DICT_LITERAL = 'dict-literal'
     NAME_REF = 'name-ref'
+    NONE_LITERAL = 'none-literal'
+    GETATTR = 'getattr'
 
 
 class Expr:
@@ -60,28 +111,48 @@ class Expr:
         return cls(ExprKind.DICT_LITERAL, operands=())
 
     @classmethod
-    def make_invoke(cls, lhs: 'Expr', name: Text, *args) -> 'Expr':
-        return cls(ExprKind.INVOKE, (lhs, name, *args))
+    def make_none(cls):
+        return cls(ExprKind.NONE_LITERAL, operands=())
+
+    @classmethod
+    def make_invoke(cls, lhs: 'Expr', args: Tuple['Expr', ...]) -> 'Expr':
+        return cls(ExprKind.INVOKE, (lhs, args))
 
     @classmethod
     def make_name_ref(cls, arg: NameDef) -> 'Expr':
+        assert isinstance(arg, NameDef), repr(arg)
         return cls(ExprKind.NAME_REF, (arg,))
+
+    @classmethod
+    def make_getattr(cls, lhs: 'Expr', name: Text) -> 'Expr':
+        return cls(ExprKind.GETATTR, (lhs, name))
 
     def __init__(self, kind: ExprKind, operands: Tuple['Expr', ...]):
         assert isinstance(operands, tuple), operands
         self.kind = kind
         self.operands = operands
 
-    def format(self) -> Text:
+    def __repr__(self) -> Text:
+        return f'{self.__class__.__name__}({self.kind}, {self.operands})'
+
+    def _format(self) -> Text:
         if self.kind == ExprKind.DICT_LITERAL:
-            assert not self.operands
             return '{}'
+        if self.kind == ExprKind.NONE_LITERAL:
+            return 'None'
         if self.kind == ExprKind.NAME_REF:
             name_def = self.operands[0]
-            assert isinstance(name_def, NameDef), name_def
+            assert isinstance(name_def, NameDef), repr(name_def)
             return '{}'.format(name_def.name)
         if self.kind == ExprKind.INVOKE:
-            return '{}.{}({})'.format(
-                self.operands[0].format(), self.operands[1],
-                ', '.join(o.format() for o in self.operands[2:]))
+            lhs, args = self.operands
+            assert isinstance(args, tuple), args
+            return '{}({})'.format(
+                lhs.format(), ', '.join(a.format() for a in args))
+        if self.kind == ExprKind.GETATTR:
+            return '({}).{}'.format(self.operands[0].format(),
+                                    self.operands[1])
         raise NotImplementedError(self)
+
+    def format(self, indent: int = 0) -> Text:
+        return textwrap.indent(self._format(), ' ' * indent)
--- tests/fuzz_ast_test.py
+++ tests/fuzz_ast_test.py
@@ -1,15 +1,54 @@
-from echo.fuzz.genseq import Statement, Expr, Block, NameDef
+from echo.fuzz.genseq import Stmt, Expr, Block, NameDef
 
 
 def test_assign_stmt():
     x = NameDef('x')
-    s0 = Statement.make_assign(x, Expr.make_dict_literal())
+    s0 = Stmt.make_assign(x, Expr.make_dict_literal())
     y = NameDef('y')
-    s1 = Statement.make_assign(y, Expr.make_dict_literal())
-    s2 = Statement.make_expr(Expr.make_invoke(Expr.make_name_ref(x), 'update',
-                             Expr.make_name_ref(y)))
+    s1 = Stmt.make_assign(y, Expr.make_dict_literal())
+    s2 = Stmt.make_expr(
+        Expr.make_invoke(
+            Expr.make_getattr(Expr.make_name_ref(x), 'update'),
+            (Expr.make_name_ref(y),)))
     b = Block((s0, s1, s2))
     assert b.format() == """\
 x = {}
 y = {}
-x.update(y)"""
+(x).update(y)
+"""
+
+
+def test_fn_stmt():
+    s0 = Stmt.make_fn_def(NameDef('my_func'), (), (
+        Stmt.make_return(Expr.make_none()),
+    ))
+    assert s0.format() == """\
+def my_func():
+    return None
+"""
+
+
+def test_class_stmt():
+    s0 = Stmt.make_class_def(NameDef('MyClass'), (
+        Stmt.make_fn_def(NameDef('my_func'), (NameDef('self'),), (
+            Stmt.make_return(Expr.make_none()),
+        )),
+    ))
+    assert s0.format() == """\
+class MyClass:
+    def my_func(self):
+        return None
+"""
+
+
+def test_invoke_class():
+    mc = NameDef('MyClass')
+    s0 = Stmt.make_class_def(mc, (Stmt.make_pass(),))
+    s1 = Stmt.make_assign(NameDef('o'),
+                          Expr.make_invoke(Expr.make_name_ref(mc), ()))
+    b = Block((s0, s1))
+    assert b.format() == """\
+class MyClass:
+    pass
+o = MyClass()
+"""
