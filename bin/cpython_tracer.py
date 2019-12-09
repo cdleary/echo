b3b9c9f346a1607f3db0334ece668923f22b986f
Christopher D. Leary <cdleary@gmail.com>
2019-12-09 00:10:37 -0800

    Record info in CPython tracer.

--- bin/cpython_tracer.py
+++ bin/cpython_tracer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import collections
 import ctypes
 import dis
+import functools
 import inspect
 import optparse
 import pprint
@@ -9,12 +11,57 @@ import sys
 import types
 from typing import Any, cast
 
-from termcolor import cprint
+import termcolor
 
 from echo import bytecode_trace
 
 
-TRACE_DUMPER = bytecode_trace.FakeTraceDumper()
+OPCODE_COUNT = 0
+
+
+class CountingTraceDumper:
+
+    def __init__(self):
+        self.counts = collections.Counter()
+        self.leaf_calls = collections.Counter()
+        self.code_stack = []
+
+    def note_call(self, code):
+        if self.code_stack:
+            self.code_stack[-1][1] = True
+        self.code_stack.append([code, False])
+
+    def note_return(self, code):
+        this_code, saw_call = self.code_stack.pop()
+        assert this_code is code
+        if not saw_call:
+            self.leaf_calls[code] += 1
+
+    def note_instruction(self, i: dis.Instruction, ctf: 'CtypeFrame'):
+        opname = i.opname
+        if opname in ('LOAD_ATTR', 'LOAD_METHOD', 'FOR_ITER') or opname.startswith('UNARY_'):
+            key = (opname, type(ctf.get_tos_value()))
+        elif opname == 'BINARY_SUBSCR':
+            key = (opname, type(ctf.get_tos_value(0)), type(ctf.get_tos_value(1)))
+        elif opname == 'COMPARE_OP':
+            key = (opname, i.argval)
+        else:
+            key = i.opname
+        self.counts[key] += 1
+        if (OPCODE_COUNT+1) % 10000 == 0:
+            self.dump()
+
+    def dump(self):
+        print('===')
+        for code, count in self.leaf_calls.most_common(10):
+            print('-- call count: {:8} bytecodes: {:5} code: {}'.format(count, len(tuple(dis.get_instructions(code))), code))
+            #dis.dis(code)
+
+        pprint.pprint(self.counts.most_common(20))
+
+
+#TRACE_DUMPER = bytecode_trace.FakeTraceDumper()
+TRACE_DUMPER = CountingTraceDumper()
 
 
 # pytype: disable=base-class-error
@@ -27,8 +74,10 @@ class _PyTryBlock(ctypes.Structure):
 # pytype: enable=base-class-error
 
 
-def print(*args, **kwargs): pass
-def cprint(*args, **kwargs): pass
+eprint = functools.partial(print, file=sys.stderr)
+cprint = functools.partial(termcolor.cprint, file=sys.stderr)
+#def print(*args, **kwargs): pass
+#def cprint(*args, **kwargs): pass
 
 
 class CtypeFrame:
@@ -113,7 +162,7 @@ class CtypeFrame:
         f_iblock = ctypes.cast(
             self.frame_id,
             ctypes.POINTER(ctypes.c_uint))[112//self.UINT_SIZE_IN_BYTES]
-        print('f_iblock:', f_iblock)
+        eprint('f_iblock:', f_iblock)
 
         type_to_str = {
             257: 'EXCEPT_HANDLER',
@@ -129,12 +178,18 @@ class CtypeFrame:
             ptb = f_blockstack[i]
             type_str = type_to_str[ptb.b_type]
             handler = -1 if ptb.b_handler == 0xffffffff else ptb.b_handler
-            print(' blockstack %d: type: %s handler: %s level: %d' % (
+            eprint(' blockstack %d: type: %s handler: %s level: %d' % (
                     i, type_str, handler, ptb.b_level))
             block_stack.append(bytecode_trace.BlockStackEntry(
                 type_str, handler, ptb.b_level))
 
         TRACE_DUMPER.note_block_stack(block_stack)
+
+    def get_tos_value(self, i=0):
+        value = self.get_value_stack_as_ptr()[i]
+        if value == 0:
+            return None
+        return self._id2obj(value)
 
     def print_stack(self):
         # Left in, just in case there's some need to look at the pointer
@@ -153,7 +208,7 @@ class CtypeFrame:
         localsplus_items = (
             (self.get_stack_top() - self.get_localsplus_start()) //
             self.ULONG_SIZE_IN_BYTES)
-        print(' localsplus_items sans stack:', localsplus_items-stack_items)
+        eprint(' localsplus_items sans stack:', localsplus_items-stack_items)
         for i in range(localsplus_items-stack_items):
             localsplus_ptr = self.get_localsplus_start_as_ptr()
             value = localsplus_ptr[i]
@@ -165,82 +220,89 @@ class CtypeFrame:
                 cprint('  LP{}: {!r} :: {!r}'.format(i, type(obj), obj),
                        file=sys.stderr, color='red')
 
-        print(' stack (%d):' % stack_items, file=sys.stderr)
+        eprint(' stack (%d):' % stack_items)
         for i in range(stack_items):
             stack_value = self.get_value_stack_as_ptr()[i]
             if stack_value == 0:
-                print('  TOS%d: <null>' % i, file=sys.stderr)
+                eprint('  TOS%d: <null>' % i)
             else:
                 obj = self._id2obj(stack_value)
-                print('  TOS%d: %r ::' % (i, type(obj)), repr(obj), '::', id(obj),
-                      file=sys.stderr)
+                eprint('  TOS%d: %r ::' % (i, type(obj)), repr(obj), '::', id(obj))
                 if isinstance(obj, types.CodeType):
-                    print('    co_varnames: {!r}'.format(obj.co_varnames))
-                    print('    co_cellvars: {!r}'.format(obj.co_cellvars))
+                    eprint('    co_varnames: {!r}'.format(obj.co_varnames))
+                    eprint('    co_cellvars: {!r}'.format(obj.co_cellvars))
                 if isinstance(obj, types.FunctionType):
-                    print('    closure:     {!r}'.format(obj.__closure__),
-                          file=sys.stderr)
-                    print('    co_cellvars: {!r}'.format(
-                            obj.__code__.co_cellvars), file=sys.stderr)
-                    print('    co_freevars: {!r}'.format(
-                            obj.__code__.co_freevars), file=sys.stderr)
+                    eprint('    closure:     {!r}'.format(obj.__closure__))
+                    eprint('    co_cellvars: {!r}'.format(
+                            obj.__code__.co_cellvars))
+                    eprint('    co_freevars: {!r}'.format(
+                            obj.__code__.co_freevars))
         self.print_block_stack()
 
 
+def _note_opcode_event(frame, opcodeno) -> None:
+    # From the docs:
+    #     The interpreter is about to execute a new opcode (see dis for
+    #     opcode details). The local trace function is called; arg is None;
+    #     the return value specifies the new local trace function.
+    #     Per-opcode events are not emitted by default: they must be
+    #     explicitly requested by setting f_trace_opcodes to True on the
+    #     frame.
+    # -- https://docs.python.org/3/library/sys.html#sys.settrace
+    #eprint('opcode about to execute...')
+    ctf = CtypeFrame(frame)
+    #eprint(' frame.f_lasti:', frame.f_lasti)
+    instructions = dis.get_instructions(frame.f_code)
+    instruction = next(inst for inst in instructions
+                       if inst.offset == frame.f_lasti)
+    TRACE_DUMPER.note_instruction(instruction, ctf)
+    #cprint('i: {} code: {}; lineno: {}'.format(opcodeno, frame.f_code, frame.f_lineno),
+    #       color='blue')
+    #cprint(' instruction: {}'.format(instruction), color='yellow')
+    #locals_ = dict(frame.f_locals)
+    #for name in ['__builtins__']:
+    #    if name in locals_:
+    #        del locals_[name]
+    #eprint(' frame.f_locals:', locals_)
+    #try:
+    #    eprint(' stack effect:', dis.stack_effect(instruction.opcode,
+    #          instruction.arg))
+    #except ValueError:
+    #    pass
+    #ctf.print_stack()
+
+
 def note_trace(frame, event, arg):
-    print('---', file=sys.stderr)
+    global OPCODE_COUNT
+    #eprint('--- trace')
 
     def print_frame_info():
-        print(' frame.f_lasti:', frame.f_lasti, file=sys.stderr)
-        print(' frame.f_lineno:', frame.f_lineno, file=sys.stderr)
-        # print(' frame.f_locals:', frame.f_locals, file=sys.stderr)
+        eprint(' frame.f_lasti:', frame.f_lasti)
+        eprint(' frame.f_lineno:', frame.f_lineno)
+        # eprint(' frame.f_locals:', frame.f_locals)
 
     if event == 'call':
+        TRACE_DUMPER.note_call(frame.f_code)
         filename = frame.f_code.co_filename
-        print('CALLING:', filename)
+        #eprint('CALLING:', filename)
         # Turn on opcode tracing for the frame we're entering.
         if not filename.startswith('<frozen'):
             frame.f_trace_opcodes = True
     elif event == 'opcode':
-        # From the docs:
-        #     The interpreter is about to execute a new opcode (see dis for
-        #     opcode details). The local trace function is called; arg is None;
-        #     the return value specifies the new local trace function.
-        #     Per-opcode events are not emitted by default: they must be
-        #     explicitly requested by setting f_trace_opcodes to True on the
-        #     frame.
-        # -- https://docs.python.org/3/library/sys.html#sys.settrace
-        print('opcode about to execute...', file=sys.stderr)
-        ctf = CtypeFrame(frame)
-        print(' frame.f_lasti:', frame.f_lasti, file=sys.stderr)
-        instructions = dis.get_instructions(frame.f_code)
-        instruction = next(inst for inst in instructions
-                           if inst.offset == frame.f_lasti)
-        TRACE_DUMPER.note_instruction(instruction)
-        cprint(' code: {}; lineno: {}'.format(frame.f_code, frame.f_lineno),
-               color='blue', file=sys.stderr)
-        cprint(' instruction: {}'.format(instruction),
-               color='yellow', file=sys.stderr)
-        locals_ = dict(frame.f_locals)
-        for name in ['__builtins__']:
-            if name in locals_:
-                del locals_[name]
-        print(' frame.f_locals:', locals_, file=sys.stderr)
-        try:
-            print(' stack effect:', dis.stack_effect(instruction.opcode,
-                  instruction.arg), file=sys.stderr)
-        except ValueError:
-            pass
-        ctf.print_stack()
+        _note_opcode_event(frame, OPCODE_COUNT)
+        OPCODE_COUNT += 1
+        pass
     elif event == 'line':
-        print('line!', file=sys.stderr)
-        print_frame_info()
+        #eprint('line!')
+        #print_frame_info()
+        pass
     elif event == 'return':
+        TRACE_DUMPER.note_return(frame.f_code)
         pass
     elif event == 'exception':
         pass
     else:
-        print('unhandled event:', event, file=sys.stderr)
+        eprint('unhandled event:', event)
         sys.exit(-1)
 
     return note_trace
@@ -268,9 +330,12 @@ def main():
     exec(contents, globals_)
     sys.settrace(None)
 
+    print('opcode count:', OPCODE_COUNT)
+
     if opts.dump_trace:
         print('Dumping', len(TRACE_DUMPER.entries), 'trace entries...')
-        TRACE_DUMPER.dump()
+
+    TRACE_DUMPER.dump()
 
 
 if __name__ == '__main__':
--- py_samples/knownf_import_numpy.py
+++ py_samples/knownf_import_numpy.py
@@ -1,3 +1,6 @@
 import numpy
 
 assert type(numpy.__version__) == str, numpy.__version__
+two = numpy.array([2])
+print(two + two)
+print(numpy.__version__)
