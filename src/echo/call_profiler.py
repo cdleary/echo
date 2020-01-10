273d1d3ec9b296c52a09101325816fc4e0fd981e
Christopher D. Leary <cdleary@gmail.com>
2020-01-10 09:17:26 -0800

    Some tweaks to call profiler.

--- src/echo/call_profiler.py
+++ src/echo/call_profiler.py
@@ -25,13 +25,19 @@ class CallProfiler:
         if isinstance(f, EFunction):
             self.profile[f][self._get_types(args)] += 1
 
+    def _code_pos(self, f):
+        if isinstance(f, EFunction):
+            return f'{f.code.co_filename}:{f.code.co_firstlineno}'
+        else:
+            raise NotImplementedError(f)
+
     def dump(self):
         items = []
         for f, counter in self.profile.items():
             items.append((sum(counter.values()), f, counter))
         items.sort(key=itemgetter(0), reverse=True)
         for _, f, counter in items:
-            print(f'{f}:')
+            print(f'{f}: {self._code_pos(f)}')
             for sig, count in sorted(
                     counter.items(), key=itemgetter(1), reverse=True):
                 print(f'  {count:8d}: {sig}')
